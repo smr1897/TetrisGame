febc15c305c916f93efa5c449bba10e6e0d9ad29
sahan <sahan.m.rajapaksha@gmail.com>
2023-06-05 12:49:12 +0530

    Add project files.

---
+++ TetrisGame/Position.cs
@@ -0,0 +1,19 @@
+﻿namespace TetrisGame
+{
+    public class Position
+    {
+        public int Row { get; set; }
+        public int Column { get; set; }
+
+        public Position(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+
+
+    }
+
+    
+
+}
